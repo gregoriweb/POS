fe9f435786d3ab7722b8d967da6b09e1c472b016
Gregori <gregoriweb@gmail.com>
2022-06-28 13:25:39 -0300

    Processament exe aula 4 classe municipioexpectativa 1

--- Exercicios_Programas/Python_Processamento_Dados/Exercicios_Programas/Exercicio_Aula4/OO/Exercicio_Aula4.py
+++ Exercicios_Programas/Python_Processamento_Dados/Exercicios_Programas/Exercicio_Aula4/OO/Exercicio_Aula4.py
@@ -5,52 +5,55 @@
     c) Gerar um grafico com o municipio com maior expectativa.
     d) Gerar um grafico com o municipio com menor expectativa.
 '''
-import FonteDados
-import OO.DestinoDados
+from FonteDados import FonteDados
+#import OO.DestinoDados
+
+class Municipio():
+
+    def __init__(self):
+        self.municipio = ""
+        self.expectativa = 0.00
 
 
-class MunicipoExpectativa():
+class MunicipioExpectativa():
 
     def __init__(self):
         self.municipios = {}
 
-    def expectativa_municipios (self, arquivo="MunicipiosExpectativa.txt"):
+    def ev_municipios (self, arquivo="MunicipiosExpectativa.txt"):
+
         fd = FonteDados()
         fd.vetorArquivo(arquivo)
-        municipio={}
-        
+        municipios = {}
+
         for i in fd.dataset:
-            cb = CampeonatoBrasileiro()
-            cb.rodada           = i[0]
-            cb.data_jogo        = i[1]
-            cb.horario          = i[2]
-            cb.dia_jogo         = i[3]
-            cb.time_mandante    = i[4]
-            cb.time_visitante   = i[5]
-            cb.time_vencedor    = i[6]
-            cb.campo            = i[7]
-            cb.placar_mandante  = i[8]
-            cb.placar_visitante = i[9]
-            cb.estado_mandante  = i[10]
-            cb.estado_visitante = i[11]
-            cb.estado_vencedor  = i[12]
-            cb.ano_campeonato   = i[1].split("/")[2]
-            cb.linha_dados = i
-            lista_jogos.append(cb)
-        return lista_jogos
+            mun = Municipio()
+            mun.municipio = i[0]
+            mun.expectativa = i[1]
+            municipios[mun.municipio] = mun.expectativa 
+        return municipios
 
-    def vitoriasMandante(self):
-        for i in self.lista_jogos:
-            if (i.time_mandante == self.time_analisado and i.time_vencedor == self.time_analisado):
-                self.VitoriasMandante.append(i)
-                self.QtdVitoriasMandante += 1
-                self.lista_jogos_mandante.append(i.linha_dados)
-        return self.QtdVitoriasMandante
+    def ev_municipios_top10 (self,municipios):
+        return
 
-    def vitoriasVisitante(self):
-        for i in self.lista_jogos:
-            if (i.time_visitante == self.time_analisado and i.time_vencedor == self.time_analisado):
-                self.VitoriasVisitante.append(i)
-                self.QtdVitoriasVisitante += 1
-                self.lista_jogos_visitante.append(i.linha_dados)
-        return self.QtdVitoriasVisitante
+me = MunicipioExpectativa()
+me.ev_municipios()
+
+
+
+
+#    def vitoriasMandante(self):
+#        for i in self.lista_jogos:
+#            if (i.time_mandante == self.time_analisado and i.time_vencedor == self.time_analisado):
+#                self.VitoriasMandante.append(i)
+#                self.QtdVitoriasMandante += 1
+#                self.lista_jogos_mandante.append(i.linha_dados)
+#        return self.QtdVitoriasMandante
+#
+#    def vitoriasVisitante(self):
+#        for i in self.lista_jogos:
+#            if (i.time_visitante == self.time_analisado and i.time_vencedor == self.time_analisado):
+#                self.VitoriasVisitante.append(i)
+#                self.QtdVitoriasVisitante += 1
+#                self.lista_jogos_visitante.append(i.linha_dados)
+#        return self.QtdVitoriasVisitante
---
+++ Exercicios_Programas/Python_Processamento_Dados/Exercicios_Programas/Exercicio_Aula4/OO/Municipio_ExpectativaVida.py
@@ -0,0 +1,30 @@
+class CampeonatoBrasileiro:
+    rodada = None 
+    data_jogo = None 
+    horario = None 
+    dia_jogo = None 
+    time_mandante = None 
+    time_visitante = None 
+    time_vencedor = None 
+    campo = None 
+    placar_mandante = None 
+    placar_visitante = None 
+    estado_mandante = None 
+    estado_visitante = None
+    estado_vencedor = None 
+    ano_campeonato = None 
+    
+    def __init__(self):
+        self.data_jogo = ""
+        self.campo = ""
+        self.hora = ""
+        self.time_mandante = ""
+        self.time_visitante = ""
+        self.time_vencedor = "" 
+        self.campo = "" 
+        self.placar_mandante = "" 
+        self.placar_visitante = "" 
+        self.estado_mandante = "" 
+        self.estado_visitante = ""
+        self.estado_vencedor = "" 
+        self.ano_campeonato = ""
